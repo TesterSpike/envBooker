fb32efed8fab218d237b477d38038e40dbd9713d
sean.pike <pike.sean@gmail.com>
2024-09-23 09:48:36 +0100

    Fix missing key for list item - Linting error

--- app/components/environmentStatusRowComponent.tsx
+++ app/components/environmentStatusRowComponent.tsx
@@ -2,26 +2,23 @@ import {environmentData} from "@/app/types/environmentData";
 
 const environmentStatusComponent = (rows: environmentData[]) => {
     return rows.map((row) => {
-        if(row.bookingData) {
-            const shareable = (row.bookingData.shareable)? 'bg-amber-200 border' : 'border'
+        if (row.bookingData) {
+            const shareable = (row.bookingData.shareable) ? 'bg-amber-200 border' : 'border'
             return (
-                <>
-                    <tr className={shareable}>
-                        <td>{row.env}</td>
-                        <td>{row.bookingData.bookedBy}</td>
-                        <td>{new Date(row.bookingData.bookingDate).toLocaleDateString("en-uk")}</td>
-                        <td>{row.bookingData.untilTime}</td>
-                        <td>{row.bookingData.untilTime}</td>
-                        <td>{row.bookingData.shareable}</td>
-                        <td>{row.bookingData.notes}</td>
-                        <td>Remove booking</td>
-                    </tr>
-                </>
+                <tr className={shareable} key={row.env}>
+                    <td>{row.env}</td>
+                    <td>{row.bookingData.bookedBy}</td>
+                    <td>{new Date(row.bookingData.bookingDate).toLocaleDateString("en-uk")}</td>
+                    <td>{row.bookingData.untilTime}</td>
+                    <td>{row.bookingData.untilTime}</td>
+                    <td>{row.bookingData.shareable}</td>
+                    <td>{row.bookingData.notes}</td>
+                    <td>Remove booking</td>
+                </tr>
             )
         } else {
-           return (
-               <>
-                <tr>
+            return (
+                <tr key={row.env}>
                     <td>{row.env}</td>
                     <td></td>
                     <td></td>
@@ -31,8 +28,7 @@ const environmentStatusComponent = (rows: environmentData[]) => {
                     <td></td>
                     <td></td>
                 </tr>
-            </>
-           );
+            );
         }
 
     });
